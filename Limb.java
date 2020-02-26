a04579072302eca8bb3de049738ca095327c020f
Nick-Stewart7 <49692388+Nick-Stewart7@users.noreply.github.com>
2020-02-25 23:22:29 -0500

    Add files via upload
    The first program, Geppetto, is used to design a dance and embed a midi file of your choice with the sweet moves you created. Puppetto then reads this midi file and plays the midi while making those puppets move!

---
+++ Limb.java
@@ -0,0 +1,11 @@
+package edu.mccc.cos210.fp.pupp;
+
+import java.awt.image.BufferedImage;
+
+public class Limb{
+	BufferedImage bi;
+	double rot;
+	public Limb(BufferedImage bi) {
+		this.bi = bi;
+	}
+}	
---
+++ Puppetto.java
@@ -0,0 +1,9 @@
+package edu.mccc.cos210.fp.pupp;
+
+import java.awt.EventQueue;
+
+public class Puppetto {
+	public static void main(String...args) {
+		EventQueue.invokeLater(Stage::new);
+	}
+}
